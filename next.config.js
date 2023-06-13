eadeced820253a49e6c4a4e912a8ce8b7f655f1e
sieun.lee <se.lee@purple.io>
2023-06-13 15:09:54 +0900

    next.config.js 설정

--- next.config.js
+++ next.config.js
@@ -1,4 +1,30 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  output: "standalone",
+  experimental: {
+    urlImports: ["https://cake.cdn.kolon.me"],
+  },
+  images: {
+    domains: ["https://cake.cdn.kolon.me", "cake.cdn.kolon.me"],
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+const intercept = require("intercept-stdout");
+
+const interceptStdout = (text) => {
+  return text.includes("Duplicate atom key") ? "" : text;
+};
+
+intercept(interceptStdout);
