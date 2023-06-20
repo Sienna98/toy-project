6b22ce22e22461ffdc6c45f63c71a1ca68e84a20
sieun.lee <se.lee@purple.io>
2023-06-20 10:29:40 +0900

    import 자동정리 추가

--- src/Components/MainPage/index.tsx
+++ src/Components/MainPage/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { styled } from "styled-components";
 import { theme } from "@/styles/theme";
 import MainFruitBox from "../FruitBox/Main";
